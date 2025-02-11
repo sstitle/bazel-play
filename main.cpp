2e9bae763c753483b4425b429a626b94a0e657ec
Sam Title <samstitle@gmail.com>
2025-02-11 02:15:18 -0500

    Just spdlog

--- main.cpp
+++ main.cpp
@@ -1,19 +1,5 @@
 #include <spdlog/spdlog.h>
-#include <CGAL/Simple_cartesian.h>
-#include <CGAL/Point_2.h>
-
-typedef CGAL::Simple_cartesian<double> Kernel;
-typedef CGAL::Point_2<Kernel> Point_2;
-
 int main() {
     spdlog::info("Hello, World!");
-
-    // Example usage of CGAL
-    Point_2 p(1.0, 2.0);
-    Point_2 q(3.0, 4.0);
-
-    spdlog::info("Point p: ({}, {})", p.x(), p.y());
-    spdlog::info("Point q: ({}, {})", q.x(), q.y());
-
     return 0;
 }
